539664012b6442b912786a39a5a68221b8942151
Ali Sattar <alirsattar@gmail.com>
2020-07-17 13:13:39 -0400

    Setting up project scaffold

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <search-page msg="TESTING"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SearchPage from './components/SearchPage.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    SearchPage
   }
 }
 </script>
