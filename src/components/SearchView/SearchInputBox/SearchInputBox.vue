039a0e53cd1d30185923bda160ea7bd19c2d3220
Ali Sattar <alirsattar@gmail.com>
2020-07-18 17:01:50 -0400

    Search is provisionally functional, and is returning results

--- src/components/SearchView/SearchInputBox/SearchInputBox.vue
+++ src/components/SearchView/SearchInputBox/SearchInputBox.vue
@@ -1,28 +1,74 @@
 <template>
   <div id="searchinputbox">
     <h1>
-      <span class="badge badge-success">SearchInputBox</span>
+      <span class="badge badge-success">SearchInputBox TESTING</span>
     </h1>
 
-    <input type="text" name="" id="search-string">
-    &nbsp;
-    &nbsp;
-    <button class="btn btn-success">Search</button>
+    <input
+      type="text"
+      id="search-string"
+      v-model="searchString" />
+    <button
+      class="btn btn-success ml-3"
+      v-on:click="search()">Search</button>
+    <br><small id="error">{{ this.$data.errorMessage }}</small>
+
+    <br>Results:
+    <ul
+      v-for="city in this.$data.searchResults"
+      v-bind:key="city">
+        <li>{{ city }}</li>
+    </ul>
   </div>
 </template>
 
 <script>
+const axios = require("axios");
+
 export default {
   name: "SearchInputBox",
   props: {},
-  components: {}
+  data: () => {
+    return {
+      searchApiBaseUrl: "http://opentable.herokuapp.com/api/",
+      searchString: "",
+      errorMessage: '',
+      searchResults: []
+    };
+  },
+  components: {},
+  methods: {
+    search() {
+      let results;
+      const urlWithQuery = this.$data.searchApiBaseUrl + this.$data.searchString;
+
+      console.log({ urlWithQuery });
+
+      axios.get(urlWithQuery)
+        .then((searchResults) => {
+          console.log(searchResults.data);
+          results = searchResults.data.cities;
+          this.$data.searchResults = searchResults.data.cities
+        })
+        .catch((error)=> {
+          console.error(error);
+          this.$data.errorMessage = error;
+        });
+
+      return results;
+    }
+  }
 };
 </script>
 
 <style scoped>
-  #searchinputbox {
-    border: 1px solid;
-    padding: 10px;
-    margin: 10px;
-  }
+#searchinputbox {
+  border: 1px solid;
+  padding: 10px;
+  margin: 10px;
+}
+
+#error {
+  color: red;
+}
 </style>
