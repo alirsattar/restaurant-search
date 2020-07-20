1eff607acdc97adf6c9a90c88e765e7359971d2e
Ali Sattar <alirsattar@gmail.com>
2020-07-20 14:11:35 -0400

    All 3 search boxes now functional

--- src/components/SearchView/SearchInputBox/SearchInputBox.vue
+++ src/components/SearchView/SearchInputBox/SearchInputBox.vue
@@ -7,9 +7,37 @@
     <form>
       <input
         type="text"
-        id="search-string"
+        id="name"
         placeholder="Restaurant Name"
-        v-model="searchString" />
+        v-model="searchTypes.name.queryString"
+        v-on:focus="clearOtherInputFields($event)" />
+      <input
+        type="text"
+        id="city"
+        placeholder="City"
+        v-model="searchTypes.city.queryString"
+        v-on:focus="clearOtherInputFields($event)" />
+      <select
+        class="select"
+        id="state"
+        placeholder="State"
+        v-model="searchTypes.state.queryString"
+        v-on:focus="clearOtherInputFields($event)">
+          <option
+            v-for="(code, state) in usStates"
+            v-bind:key="state"
+            :value="state">{{ code }}</option>
+      </select>
+      <span class="m-3">OR</span>
+      <input
+        type="text"
+        id="zip"
+        class=""
+        maxlength="5"
+        placeholder="Zip Code"
+        pattern="[0-9]*"
+        v-model="searchTypes.zip.queryString"
+        v-on:focus="clearOtherInputFields($event)" />
       <button
         type="submit"
         class="btn btn-success ml-3"
@@ -20,7 +48,7 @@
     <div
       id="resultcount"
       v-show="this.$data.resultCount">
-      {{ this.$data.resultCount }}
+      {{ this.$data.resultCount }} Results
     </div>
     
   </div>
@@ -34,17 +62,98 @@ export default {
   props: {},
   data: () => {
     return {
-      searchApiBaseUrl: "http://opentable.herokuapp.com/api/restaurants?name=",
-      searchString: "",
+      searchApiBaseUrl: "http://opentable.herokuapp.com/api/restaurants",
+      searchTypes: {
+        name: {
+          queryParam: "?name=",
+          queryString: ""
+        },
+        city: {
+          queryParam: "?city=",
+          queryString: ""
+        },
+        state: {
+          queryParam: "?state=",
+          queryString: ""
+        },
+        zip: {
+          queryParam: "?zip=",
+          queryString: ""
+        }
+      },
+      restaurantNameQuery: "",
+      restaurantZipQuery: "",
+      restaurantCityQuery: "",
+      restaurantStateQuery: "",
+      finalQueryParam: "",
       errorMessage: '',
-      resultCount: 0
+      resultCount: 0,
+      usStates: {
+        "AL": "Alabama",
+        "AK": "Alaska",
+        "AZ": "Arizona",
+        "AR": "Arkansas",
+        "CA": "California",
+        "CO": "Colorado",
+        "CT": "Connecticut",
+        "DE": "Delaware",
+        "DC": "District of Columbia",
+        "FL": "Florida",
+        "GA": "Georgia",
+        "HI": "Hawaii",
+        "ID": "Idaho",
+        "IL": "Illinois",
+        "IN": "Indiana",
+        "IA": "Iowa",
+        "KS": "Kansas",
+        "KY": "Kentucky",
+        "LA": "Louisiana",
+        "ME": "Maine",
+        "MD": "Maryland",
+        "MA": "Massachusetts",
+        "MI": "Michigan",
+        "MN": "Minnesota",
+        "MS": "Mississippi",
+        "MO": "Missouri",
+        "MT": "Montana",
+        "NE": "Nebraska",
+        "NV": "Nevada",
+        "NH": "New Hampshire",
+        "NJ": "New Jersey",
+        "NM": "New Mexico",
+        "NY": "New York",
+        "NC": "North Carolina",
+        "ND": "North Dakota",
+        "OH": "Ohio",
+        "OK": "Oklahoma",
+        "OR": "Oregon",
+        "PA": "Pennsylvania",
+        "RI": "Rhode Island",
+        "SC": "South Carolina",
+        "SD": "South Dakota",
+        "TN": "Tennessee",
+        "TX": "Texas",
+        "UT": "Utah",
+        "VT": "Vermont",
+        "VA": "Virginia",
+        "WA": "Washington",
+        "WV": "West Virginia",
+        "WI": "Wisconsin",
+        "WY": "Wyoming"
+      }
     };
   },
   methods: {
     onSearch: function(buttonEvent) {
       buttonEvent.preventDefault();
 
-      const urlWithQuery = this.$data.searchApiBaseUrl + this.$data.searchString;
+      // const whichField = this.whichInputFieldToUse();
+
+      // TODO: Function to figure out which input field's string will be appended to URL
+      const whichField = this.whichInputFieldToUse();
+      const urlWithQuery = this.buildFinalQueryUrl(whichField);
+
+      // const urlWithQuery = this.$data.searchApiBaseUrl + this.$data.searchString;
 
       console.log({ urlWithQuery });
 
@@ -62,6 +171,54 @@ export default {
           console.error(error);
           this.$data.errorMessage = error;
         });
+    },
+    clearOtherInputFields: function(focusEvent) {
+      focusEvent.preventDefault();
+      // const inputFields = document.getElementById('searchinputbox').querySelectorAll('input');
+      // const fieldBeingFocusedId = focusEvent.srcElement.id;
+
+      // inputFields.forEach((field) => {
+      //   if(field.id !== fieldBeingFocusedId) {
+      //     console.log('starting field value: ', field.value);
+
+      //     field.value = '';
+
+      //     console.log('ending field value: ', field.value);
+      //   }
+      // })
+
+      // // TODO: Clear the other input field
+      // console.log({inputFields, fieldBeingFocusedId});
+    },
+    whichInputFieldToUse: function() {
+      let whichField;
+      
+      const allInputFields = document.getElementById('searchinputbox').querySelectorAll('input, select');
+      let fieldsWithInput = [];
+
+      allInputFields.forEach((field)=> {
+        if(field.value) {
+          fieldsWithInput.push(field.id);
+        }
+      });
+
+      if (fieldsWithInput.length) {
+        // Just take the first field with input, if multiple fields have been filled in
+        whichField = fieldsWithInput[0];
+      }
+
+      return whichField;
+    },
+    buildFinalQueryUrl: function(whichField) {
+      const baseUrl = this.$data.searchApiBaseUrl;
+      const queryParam = this.$data.searchTypes[whichField].queryParam;
+      const searchString = this.$data.searchTypes[whichField].queryString;
+
+      const finalUrl = baseUrl + queryParam + searchString;
+
+      console.log({ baseUrl, queryParam, searchString });
+
+      return finalUrl;
     }
   }
 };
--- src/components/SearchView/SearchResults/SearchResultList/SearchResultListCard/SearchResultListCard.vue
+++ src/components/SearchView/SearchResults/SearchResultList/SearchResultListCard/SearchResultListCard.vue
@@ -3,7 +3,16 @@
     <h4>
       <span class="badge badge-success">SearchResultListCard</span>
     </h4>
-    {{ theResult.name }}
+    <br>Image: <img class="restaurant-image" :src="theResult.image_url" alt="">
+    <br>Name: {{ theResult.name }}
+    <br>Distance:
+    <br>Address: {{ theResult.address }}
+    <br>City: {{ theResult.city }}
+    <br>State: {{ theResult.state }}
+    <br>Price: {{ theResult.price }}
+    <br>Stars: {{  }}
+    <br>Reservation Number: {{ theResult.phone }}
+    <br>Website: {{ theResult.reserve_url }}
   </div>
 </template>
 
@@ -28,5 +37,9 @@ export default {
   border: 1px solid;
   padding: 10px;
   margin: 0px 10px 10px 10px;
+}
+
+.restaurant-image {
+  max-height: 100px;
 }
 </style>
