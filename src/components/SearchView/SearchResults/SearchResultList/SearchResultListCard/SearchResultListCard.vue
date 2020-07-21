6e399f045b9530410d9034c3496101466754f4f3
Ali Sattar <alirsattar@gmail.com>
2020-07-21 01:22:44 -0400

    Further adjustments

--- src/components/SearchView/SearchResults/SearchResultList/SearchResultListCard/SearchResultListCard.vue
+++ src/components/SearchView/SearchResults/SearchResultList/SearchResultListCard/SearchResultListCard.vue
@@ -3,16 +3,16 @@
     <div class="row">
 
       <!-- RIGHT SIDE -->
-      <div class="col-6 col-sm-12 col-md-12 col-lg-6">
+      <div class="col-sm-12 col-md-12 col-lg-6">
         <div class="row">
 
           <!-- IMAGE CONTAINER -->
-          <div class="col-2 col-sm-12">
+          <div class="col-sm-12 col-md-6 col-lg-2">
             <img class="restaurant-image img-fluid" src="images/hungrytime_icon.jpg" alt="">
           </div>
 
           <!-- RESTAURANT INFO -->
-          <div class="col-10 col-sm-12">
+          <div class="col-sm-12 col-md-6 col-lg-10">
             <h4 class="text"><strong>{{ theResult.name }}</strong></h4>
             <h6 class="brown"><strong>Restaurant Type</strong></h6>
             <h5><i class="fas fa-map-marker-alt highlighted"></i> <span class="text">0.68 mi | {{ theResult.address }}</span></h5>
